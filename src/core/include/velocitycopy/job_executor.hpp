c135281b7ee01c3b04f50956affbe6823840e5d8
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-16 08:27:12 -0400

    core: support one-shot conflict replacement

--- src/core/include/velocitycopy/job_executor.hpp
+++ src/core/include/velocitycopy/job_executor.hpp
@@ -46,6 +46,7 @@ struct JobResult {
 struct JobExecutionOptions {
     std::uint32_t worker_count{1};
     ExistingDestinationPolicy existing_destination{ExistingDestinationPolicy::Fail};
+    std::uint64_t replace_file_id{};
 };
 
 class JobExecutor final {
