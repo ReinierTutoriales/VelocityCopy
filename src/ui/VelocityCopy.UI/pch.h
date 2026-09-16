355c96cff6d356ba35414a12819696e3986501ff
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-16 05:17:24 -0400

    fix: include WinUI data projection

--- src/ui/VelocityCopy.UI/pch.h
+++ src/ui/VelocityCopy.UI/pch.h
@@ -16,6 +16,7 @@
 #include <winrt/Microsoft.UI.Windowing.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Data.h>
 #include <winrt/Microsoft.UI.Xaml.Input.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
 #include <winrt/Microsoft.Windows.ApplicationModel.Resources.h>
