45cba74514f1e738eeee5a7b4fbbfc852a9b6f5f
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-16 04:57:12 -0400

    fix: include MRT Core resource projection

--- src/ui/VelocityCopy.UI/pch.h
+++ src/ui/VelocityCopy.UI/pch.h
@@ -18,6 +18,7 @@
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Input.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.Windows.ApplicationModel.Resources.h>
 #include <winrt/Microsoft.Windows.Storage.Pickers.h>
 
 #include <atomic>
