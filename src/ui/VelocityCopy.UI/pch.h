943badf97efd9bc65ae34ea50b82ce34e3faea87
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-17 19:19:38 -0400

    build: include WinUI text projection for queue typography

--- src/ui/VelocityCopy.UI/pch.h
+++ src/ui/VelocityCopy.UI/pch.h
@@ -12,6 +12,7 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Storage.h>
+#include <winrt/Windows.UI.Text.h>
 #include <winrt/Microsoft.UI.Dispatching.h>
 #include <winrt/Microsoft.UI.Windowing.h>
 #include <winrt/Microsoft.UI.Xaml.h>
