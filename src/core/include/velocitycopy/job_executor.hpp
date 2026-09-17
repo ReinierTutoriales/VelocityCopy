13ed020c267ca0f0069380664e0d1fbddc198ee9
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-17 18:32:07 -0400

    core: preserve adaptive strategy metadata in execution options

--- src/core/include/velocitycopy/job_executor.hpp
+++ src/core/include/velocitycopy/job_executor.hpp
@@ -46,6 +46,9 @@ struct JobResult {
 struct JobExecutionOptions {
     std::uint32_t worker_count{1};
     std::uint32_t copy_flags{};
+    CopyStrategyKind strategy{CopyStrategyKind::WindowsCopyFile2};
+    std::uint32_t suggested_buffer_bytes{};
+    bool async_iocp_candidate{};
     ExistingDestinationPolicy existing_destination{ExistingDestinationPolicy::Fail};
     std::uint64_t replace_file_id{};
 };
