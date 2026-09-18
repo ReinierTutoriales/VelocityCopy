6ee93f6339b4e90a6235759056ee641580cfd3c9
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-18 07:58:06 -0400

    core: expose documented storage device identity

--- src/core/include/velocitycopy/storage_profiler.hpp
+++ src/core/include/velocitycopy/storage_profiler.hpp
@@ -19,8 +19,11 @@ struct StorageProfile {
     std::filesystem::path volume_root;
     std::uint32_t logical_sector_bytes{};
     std::uint32_t physical_sector_bytes{};
+    std::uint32_t device_type{};
+    std::uint32_t device_number{};
     bool remote{};
     bool sector_info_available{};
+    bool device_number_available{};
     bool incurs_seek_penalty{};
     bool seek_penalty_available{};
 };
