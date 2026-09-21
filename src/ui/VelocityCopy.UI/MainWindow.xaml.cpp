dc53e7cb80d3bc2d08ace293e09278a070461751
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-21 19:48:14 -0400

    Add skip and stop accessibility labels

--- src/ui/VelocityCopy.UI/MainWindow.xaml.cpp
+++ src/ui/VelocityCopy.UI/MainWindow.xaml.cpp
@@ -40,10 +40,16 @@ MainWindow::MainWindow() {
     try {
         Microsoft::Windows::ApplicationModel::Resources::ResourceLoader loader;
         const auto pause = loader.GetString(L"ActionPause");
+        const auto skip = loader.GetString(L"ActionSkip");
+        const auto stop = loader.GetString(L"ActionStop");
         const auto cancel = loader.GetString(L"ActionCancel");
         ToolTipService::SetToolTip(PauseButton(), box_value(pause));
+        ToolTipService::SetToolTip(SkipButton(), box_value(skip));
+        ToolTipService::SetToolTip(StopButton(), box_value(stop));
         ToolTipService::SetToolTip(CancelButton(), box_value(cancel));
         Microsoft::UI::Xaml::Automation::AutomationProperties::SetName(PauseButton(), pause);
+        Microsoft::UI::Xaml::Automation::AutomationProperties::SetName(SkipButton(), skip);
+        Microsoft::UI::Xaml::Automation::AutomationProperties::SetName(StopButton(), stop);
         Microsoft::UI::Xaml::Automation::AutomationProperties::SetName(CancelButton(), cancel);
 
         const auto move_up = loader.GetString(L"ActionMoveUp");
