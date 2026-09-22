d0e5b52313e1a022f7129d25d69ab1b1bd885fe4
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-22 09:09:42 -0400

    Add core StorageKey resolver API (7b)

--- src/core/include/velocitycopy/transfer_router.hpp
+++ src/core/include/velocitycopy/transfer_router.hpp
@@ -10,6 +10,7 @@ namespace velocitycopy {
 enum class RouteDecision { StartNew, AppendTo, WaitFor, Ask };
 enum class RouteChoice { Append, Wait, Parallel };
 struct StorageKey { std::wstring volume; std::optional<std::uint32_t> disk; };
+[[nodiscard]] StorageKey resolve_storage_key(const std::filesystem::path&) noexcept;
 struct ActiveSession { std::uint64_t window_id{}; std::filesystem::path destination_root; FileOperation operation{FileOperation::Copy}; bool accepting_appends{}; StorageKey destination, source; };
 struct TransferRequest { std::filesystem::path destination_root; FileOperation operation{FileOperation::Copy}; StorageKey destination, source; };
 struct RoutePreferences { std::optional<RouteChoice> same_destination; std::optional<RouteChoice> same_device; };
