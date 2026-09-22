efc0b4b8c492867fc9dbda441e911bb79cf59051
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-22 04:12:19 -0400

    Make App own global tray

--- src/ui/VelocityCopy.UI/App.xaml.h
+++ src/ui/VelocityCopy.UI/App.xaml.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "App.xaml.g.h"
+#include "AppTray.h"
 
 #include "velocitycopy/efficiency_coordinator.hpp"
 #include "velocitycopy/ipc_transport.hpp"
@@ -17,10 +18,13 @@ struct App : AppT<App> {
     void RemoveEfficiencyVote(std::uint64_t window_id) noexcept;
     void SetShuttingDown(bool value) noexcept;
     std::uint64_t NextWindowId() noexcept;
+    void ShowPrimaryWindow();
+    void ExitFromTray() noexcept;
 
 private:
     void ApplyEfficiencyMode(bool enabled) noexcept;
     Microsoft::UI::Xaml::Window window_{nullptr};
+    AppTray tray_;
     std::unique_ptr<velocitycopy::SingleInstance> instance_;
     std::shared_ptr<velocitycopy::ShellIpcServer> server_;
     std::jthread ipc_thread_;
