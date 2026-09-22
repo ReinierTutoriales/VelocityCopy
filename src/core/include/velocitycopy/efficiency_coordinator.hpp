e394e18f907a497cce5fb8b2ad8994554a135ce2
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-21 21:14:19 -0400

    Add process efficiency coordinator

---
+++ src/core/include/velocitycopy/efficiency_coordinator.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstdint>
+#include <unordered_map>
+
+namespace velocitycopy {
+class EfficiencyCoordinator {
+public:
+    bool update(std::uint64_t window_id, bool eligible);
+    bool remove(std::uint64_t window_id);
+    bool set_shutting_down(bool value);
+    [[nodiscard]] bool desired() const noexcept;
+private:
+    std::unordered_map<std::uint64_t, bool> votes_;
+    bool shutting_down_{};
+};
+} // namespace velocitycopy
