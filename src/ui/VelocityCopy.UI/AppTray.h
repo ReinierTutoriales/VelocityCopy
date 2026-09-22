dd7aa56fe947614c975fdc749c5ad9231545a07f
ReinierTutoriales <86568548+ReinierTutoriales@users.noreply.github.com>
2026-09-22 04:12:08 -0400

    Add App-owned tray host

---
+++ src/ui/VelocityCopy.UI/AppTray.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <windows.h>
+#include <shellapi.h>
+
+namespace winrt::VelocityCopyUI::implementation {
+struct App;
+
+class AppTray final {
+public:
+    AppTray() = default;
+    ~AppTray();
+    AppTray(const AppTray&) = delete;
+    AppTray& operator=(const AppTray&) = delete;
+
+    bool Initialize(App* owner) noexcept;
+    void Remove() noexcept;
+    [[nodiscard]] HWND hwnd() const noexcept { return hwnd_; }
+
+private:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
+    LRESULT HandleMessage(UINT message, WPARAM wparam, LPARAM lparam) noexcept;
+    void ShowMenu(POINT anchor) noexcept;
+    void RestoreIcon() noexcept;
+
+    App* owner_{};
+    HWND hwnd_{};
+    HICON icon_{};
+    NOTIFYICONDATAW data_{};
+    UINT taskbar_created_message_{};
+    bool added_{};
+    bool v4_{};
+};
+}
